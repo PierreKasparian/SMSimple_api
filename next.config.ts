e3f5f17dc3e422aa9577d775362484e6080f5ede
kaspa <kasparianpierre@gmail.com>
2025-04-12 15:08:56 +0200

    svp

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async headers() {
-    const headers = [];
-    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-      headers.push({
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noindex",
-          },
-        ],
-        source: "/:path*",
-      });
-    }
-    return headers;
-  },
+  // async headers() {
+  //   const headers = [];
+  //   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+  //     headers.push({
+  //       headers: [
+  //         {
+  //           key: "X-Robots-Tag",
+  //           value: "noindex",
+  //         },
+  //       ],
+  //       source: "/:path*",
+  //     });
+  //   }
+  //   return headers;
+  // },
 
   rewrites: async () => {
     return [
