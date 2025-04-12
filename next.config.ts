7aecbf976257d97f6d7780077408bb335f28a8c4
kaspa <kasparianpierre@gmail.com>
2025-04-12 15:25:16 +0200

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // async headers() {
-  //   const headers = [];
-  //   if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
-  //     headers.push({
-  //       headers: [
-  //         {
-  //           key: "X-Robots-Tag",
-  //           value: "noindex",
-  //         },
-  //       ],
-  //       source: "/:path*",
-  //     });
-  //   }
-  //   return headers;
-  // },
-
-  rewrites: async () => {
-    return [
-      {
-        source: "/sms-api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:5328/sms-api/:path*"
-            : "/sms-api/",
-      },
-    ];
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview") {
+      headers.push({
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+        source: "/:path*",
+      });
+    }
+    return headers;
   },
+
 };
 
 export default nextConfig;
